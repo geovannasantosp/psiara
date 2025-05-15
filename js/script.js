f44a14f05c8832885f9255fe4c625d29a6542f99
gee_santosp <geovanna.gps.veleiros@gmail.com>
2025-05-15 16:22:12 -0300

    att fotos

--- js/script.js
+++ js/script.js
@@ -42,11 +42,68 @@
   })
 
   
+/*  EFEITO DIGITAÇÃO */
+const typed = select(".typed");
+
+if(typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+typed_strings = typed_strings.split(',')
+
+new Typed('.typed', {
+    strings: typed_strings,
+    loop: true, //infinito
+    typeSpeed: 80, //velocidade digitação
+    backSpeed: 50, //apaga
+    backDelay: 1000, // tempo de espera
 
 
+})
+
+}
+
+
+  new PureCounter();
   
-  
+ 
+  /* SKILLS */
+  let skilsContent = select('.skills-content');
+  if(skilsContent) { /* verifica o elemento */
+    new Waypoint({ /* cria uma instancia */
+        element: skilsContent,
+        offset:'80%', /* janela de visualização */
+        handler:function(direction){
+            let progress = select('.progress .progress-bar', true);
+            progress.forEach((el) => {
+                el.style.width = el.getAttribute('aria-valuenow') + '%'
+            });
+        }
+    })
+  }
 
+  /* Isotope - FILTROS */
 
+  window.addEventListener( 'load', () => {
+    let portfolioContainer = select(".portfolio-container")
+    if (portfolioContainer) {
+        let portfolioIsotope = new Isotope(portfolioContainer,{
+            itemsSelector: '.portfolio-item'
+        });
+        let portfolioFilters = select('portfolio-filters li', true);
+
+        on('click', '#portfolio-filters li', function(e) {
+            e.preventDefault();
+            portfolioFilters.forEach(function(el){
+                el.classList.remove('filter-active');
+            });
+        
+            this.classList.add('filter-active');
+
+            portfolioIsotope.arrange({
+                filter: this.getAttribute('data-filter')
+            });
+        }, true);
+    }
+
+  });
 
 })()
