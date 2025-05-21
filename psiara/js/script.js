d8e6ada25699d88ba907ef3a952dc70ce490b645
gee_santosp <geovanna.gps.veleiros@gmail.com>
2025-05-21 16:02:23 -0300

    att imagens

--- psiara/js/script.js
+++ psiara/js/script.js
@@ -61,9 +61,53 @@ new Typed('.typed', {
 
 }
 
-  
-  
 
+  new PureCounter();
+  
+ 
+  /* SKILLS */
+  let skilsContent = select('.skills-content');
+  if(skilsContent) { /* verifica o elemento */
+    new Waypoint({ /* cria uma instancia */
+        element: skilsContent,
+        offset:'80%', /* janela de visualização */
+        handler:function(direction){
+            let progress = select('.progress .progress-bar', true);
+            progress.forEach((el) => {
+                el.style.width = el.getAttribute('aria-valuenow') + '%'
+            });
+        }
+    })
+  }
 
+  /* Isotope - FILTROS */
+
+  window.addEventListener( 'load', () => {
+    let portfolioContainer = select(".portfolio-container")
+    if (portfolioContainer) {
+        let portfolioIsotope = new Isotope(portfolioContainer,{
+            itemsSelector: '.portfolio-item'
+        });
+        let portfolioFilters = select('portfolio-filters li', true);
+
+        on('click', '#portfolio-filters li', function(e) {
+            e.preventDefault();
+            portfolioFilters.forEach(function(el){
+                el.classList.remove('filter-active');
+            });
+        
+            this.classList.add('filter-active');
+
+            portfolioIsotope.arrange({
+                filter: this.getAttribute('data-filter')
+            });
+        }, true);
+    }
+
+  });
+
+  const portfolioLightbox = Glightbox({
+    selector: '.portfolio-lightbox'
+  });
 
 })()
